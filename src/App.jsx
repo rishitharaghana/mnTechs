f170ef4dfd9d57d2e9e55ef9041aca7993b3d6a8
Muthyalanayudu-Maddu <muthyalanayudu.marketwings@gmail.com>
2025-06-09 14:29:17 +0530

    added the initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        MN tech landing page
+      </div>
+    </>
+  )
+}
+
+export default App

64533ea1fcc4e7183189b7968344c286aab46811
Muthyalanayudu-Maddu <muthyalanayudu.marketwings@gmail.com>
2025-06-13 14:33:23 +0530

    added content in Index

--- src/pages/Home/Index.jsx
+++ src/pages/Home/Index.jsx
@@ -13,20 +13,42 @@ const ThinkingSection = lazy(() => import("../../Components/ThinkingSection"));
 const Navigation = lazy(() => import("../../Components/Navigation"));
 const Contacts = lazy(() => import("../../Components/Contacts"));
 
+
 export default function Index() {
   return (
+    <>
+    <Navigation />
     <Suspense fallback={<div className="flex items-center justify-center h-screen">Loading...</div>}>
-      <Navigation />
       <Hero />
+    </Suspense>
+    <Suspense fallback={<div className="flex items-center justify-center h-screen">Loading...</div>}>
       <ItServices />
+    </Suspense>
+    <Suspense fallback={<div className="flex items-center justify-center h-screen">Loading...</div>}>
       <CollaborationSection />
+    </Suspense>
+    <Suspense fallback={<div className="flex items-center justify-center h-screen">Loading...</div>}>
       <LatestProjects />
+    </Suspense>
+    <Suspense fallback={<div className="flex items-center justify-center h-screen">Loading...</div>}>
       <SkillsSection />
+    </Suspense>
+    <Suspense fallback={<div className="flex items-center justify-center h-screen">Loading...</div>}>
       <ThinkingSection />
+    </Suspense>
+    <Suspense fallback={<div className="flex items-center justify-center h-screen">Loading...</div>}>
       <TestimonialsSection />
+    </Suspense>
+    <Suspense fallback={<div className="flex items-center justify-center h-screen">Loading...</div>}>
       <ClientLogos />
+    </Suspense>
+    <Suspense fallback={<div className="flex items-center justify-center h-screen">Loading...</div>}>
       <Contacts />
+    </Suspense>
+    <Suspense fallback={<div className="flex items-center justify-center h-screen">Loading...</div>}>
       <Footer />
     </Suspense>
+    </>
+    
   );
 }

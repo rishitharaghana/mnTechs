aa7b66df886e2355e91c60ad90a0d49bd99d71b4
Atike hemasri <atikehemasri@gmail.com>
2025-06-23 10:34:09 +0530

    dropdown

--- src/Hooks/axiosInstance.js
+++ src/Hooks/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const ngrokAxiosInstance = axios.create({
-  baseURL: 'https://b247-45-112-30-210.ngrok-free.app',
+  baseURL: 'https://1277-110-235-236-221.ngrok-free.app',
   headers: {
     'Content-Type': 'application/json',
     'ngrok-skip-browser-warning': 'true',

ca0b97bd857166d9ae57397636f8cca248786a0d
Muthyalanayudu-Maddu <muthyalanayudu.marketwings@gmail.com>
2025-06-24 12:40:49 +0530

    API added

--- src/Hooks/axiosInstance.js
+++ src/Hooks/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const ngrokAxiosInstance = axios.create({
-  baseURL: 'https://9946-2401-4900-674b-3d77-716b-155e-7798-3209.ngrok-free.app',
+  baseURL: 'https://6432-2409-40f0-119a-8401-5c1f-a1bc-5a11-de0a.ngrok-free.app',
   headers: {
     'Content-Type': 'application/json',
     'ngrok-skip-browser-warning': '69420',

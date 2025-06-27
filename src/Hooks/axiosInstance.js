ce3d45432503ec131a97a291c497a89856c2227f
rishitharaghana <jrishitha2001@gmail.com>
2025-06-27 13:00:15 +0530

    added

--- src/Hooks/axiosInstance.js
+++ src/Hooks/axiosInstance.js
@@ -2,10 +2,17 @@ import axios from 'axios';
 
 
 const ngrokAxiosInstance = axios.create({
-  baseURL: 'https://api.mntechs.com',
+  baseURL: 'https://26cf-110-235-236-151.ngrok-free.app',
   headers: {
     'Content-Type': 'application/json',
+    'ngrok-skip-browser-warning': '69420',
   },
 });
+// const ngrokAxiosInstance = axios.create({
+//   baseURL: 'https://api.mntechs.com',
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+// });
 
 export default ngrokAxiosInstance;

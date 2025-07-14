7adf186edb833b89ccb99d5c98769c93c33c7524
jeevanjames2000 <jeevanjames2000@gmail.com>
2025-07-14 15:03:34 +0530

    sd

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // server:{
-  //   host:"0.0.0.0",
-  //   port:4000,
-  //   strictPort:true,
-  //   cors:{
-  //     origin:"*",
-  //     methods:["GET","POST","PUT","DELETE","OPTIONS"],
-  //     allowedHeaders:["Content-Type","Authorization"]
-  //   },
-  // }
-})
+  server: {
+    host: "0.0.0.0",
+    port: 4000,
+    strictPort: true,
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+    },
+  },
+});

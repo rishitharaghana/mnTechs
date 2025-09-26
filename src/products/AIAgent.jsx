d27776a66294f2ac28d82ebf3a66d0e2ce7245a2
muthyala10 <muthylanayudu@meetowner.in>
2025-09-26 10:10:34 +0530

    unique background option added

--- src/products/AIAgent.jsx
+++ src/products/AIAgent.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navigation from '../Components/Navigation';
-import BreadCrumb from '../pages/BreadCrumb';
+import Breadcrumb from '../pages/BreadCrumb';
 import SaasApplications from '../Services/SaasApplications';
 import ServiceFooter from '../Services/ServiceFooter';
 import MetaPage from '../pages/MetaPage';
@@ -10,13 +10,14 @@ const AIAgent = () => {
     <>
       <div>
         <Navigation />
-        <BreadCrumb 
+        <Breadcrumb
           title="AI Agent"
-          paragraph={`AI Agents That Work Around the Clock. Faster support, smarter automation, real results.`} 
+          paragraph="AI Agents That Work Around the Clock. Faster support, smarter automation, real results."
+          // backgroundImage="https://images.unsplash.com/photo-1620712943543-bcc4688e7485?ixlib=rb-4.0.3&auto=format&fit=crop&w=1170&q=80" 
         />
-        <MetaPage 
+        <MetaPage
           title="Best AI Agent Development Companies in Hyderabad"
-          description="Best AI agent development companies in Hyderabad? MNTechs offers top AI software solutions, intelligent automation, and a custom AI agent."
+          description="Best AI agent development companies in Hyderabad? MNTechs offers top AI software solutions, intelligent automation, and custom AI agents."
         />
         <SaasApplications />
         <ServiceFooter />
